113953f7eef8f0211dc05ac1059c5889c63b883d
Amalia Kivansa <amalia.kanggreansi@paracorpgroup.com>
2025-09-17 14:24:49 +0700

    feat: add route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import { Route, Routes, useLocation } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Movies from './pages/Movies';
+import MovieDetails from './pages/MovieDetails';
+import SeatLayout from './pages/SeatLayout';
+import MyBookings from './pages/MyBookings';
+import Favorite from './pages/Favorite';
 
 const App = () => {
+  const isAdminRoute = useLocation().pathname.startsWith('/admin');
   return (
-    <div>App</div>
-  )
-}
+    <>
+      {!isAdminRoute && <Navbar />}
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/movies' element={<Movies />} />
+        <Route path='/movies/:id' element={<MovieDetails />} />
+        <Route path='/movies/:id/:date' element={<SeatLayout />} />
+        <Route path='/my-bookings' element={<MyBookings />} />
+        <Route path='/favorite' element={<Favorite />} />
+      </Routes>
+    </>
+  );
+};
 
-export default App
+export default App;
